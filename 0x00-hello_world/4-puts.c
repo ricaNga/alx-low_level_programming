2b0031caa57f387c290c9ebed01c960a584f5c7b
ricaNga <27896099+ricaNga@users.noreply.github.com>
2022-04-07 07:26:32 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-  /**
-   *main - entry point
-   *
-   *Return: 0 after printing the function
-   */
-  int main(void)
-  {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
-  }
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
 
